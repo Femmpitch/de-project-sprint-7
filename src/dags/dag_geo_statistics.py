93c036ae941cc8f37cd7db7cff3e25f6611efb3e
Fedor Pesyak <fedoroffworking@gmail.com>
2026-09-10 14:28:45 +0300

    Update dag params

--- src/dags/dag_geo_statistics.py
+++ src/dags/dag_geo_statistics.py
@@ -1,9 +1,7 @@
-import airflow
-from datetime import timedelta
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 import os
-from datetime import date, datetime
+from datetime import datetime
 
 os.environ['HADOOP_CONF_DIR'] = '/etc/hadoop/conf'
 os.environ['YARN_CONF_DIR'] = '/etc/hadoop/conf'
@@ -13,21 +11,23 @@ os.environ['PYTHONPATH'] ='/usr/local/lib/python3.8'
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2022, 6, 21),
+    'start_date': datetime(2022, 6, 1),
 }
 
 dag_spark = DAG(
     dag_id = "de_project_sprint_7",
     default_args=default_args,
-    schedule_interval=None,
+    schedule='0 0 * * *', # или schedule='@daily'
+    catchup=True, 
+    max_active_runs=1
 )
 
 user_locations = SparkSubmitOperator(
     task_id='step_2_user_locations',
     dag=dag_spark,
-    application ='../scripts/step_2_user_locations.py' ,
+    application ='{{ dag_run.dag.folder }}/../scripts/step_2_user_locations.py' ,
     conn_id= 'yarn_spark',
-    application_args = ["2022-06-21", 7, "/user/s18314377/data/geo/geo.csv", 3, "/user/master/data/geo/events", "/user/s18314377/analytics"],
+    application_args = ["{{ ds }}", 7, "/user/s18314377/data/geo/geo.csv", 3, "/user/master/data/geo/events", "/user/s18314377/analytics"],
     conf={
         "spark.driver.maxResultSize": "20g"
     },
@@ -38,9 +38,9 @@ user_locations = SparkSubmitOperator(
 zone_statistics = SparkSubmitOperator(
     task_id='step_3_zone_statistics',
     dag=dag_spark,
-    application ='../scripts/step_3_zone_statistics.py' ,
+    application ='{{ dag_run.dag.folder }}/../scripts/step_3_zone_statistics.py' ,
     conn_id= 'yarn_spark',
-    application_args = ["2022-06-21", 7, "/user/s18314377/data/geo/geo.csv", "/user/master/data/geo/events", "/user/s18314377/analytics"],
+    application_args = ["{{ ds }}", 7, "/user/s18314377/data/geo/geo.csv", "/user/master/data/geo/events", "/user/s18314377/analytics"],
     conf={
         "spark.driver.maxResultSize": "20g"
     },
@@ -51,9 +51,9 @@ zone_statistics = SparkSubmitOperator(
 user_recommendations = SparkSubmitOperator(
     task_id='step_4_user_recommendations',
     dag=dag_spark,
-    application ='../scripts/step_4_user_recommendations.py' ,
+    application ='{{ dag_run.dag.folder }}/../scripts/step_4_user_recommendations.py' ,
     conn_id= 'yarn_spark',
-    application_args = ["2022-06-21", 7, "/user/s18314377/data/geo/geo.csv", 10, "/user/master/data/geo/events", "/user/s18314377/analytics"],
+    application_args = ["{{ ds }}", 7, "/user/s18314377/data/geo/geo.csv", 10, "/user/master/data/geo/events", "/user/s18314377/analytics"],
     conf={
         "spark.driver.maxResultSize": "20g"
     },
